fb75bc029e114d216de85b50fe869470377b8f18
Viktoriia Vdovina <vdovina.viskas@gmail.com>
2022-10-27 21:14:53 -0700

    source-code link

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Weather from "./Weather";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1 className="title"> Weather App</h1>
-      <Weather />
+    <div>
+      <div className="App">
+        <h1 className="title"> Weather App</h1>
+        <Weather />
+      </div>
+      <a href="https://github.com/ViktoriiaVS-20/my-app">Open-source code</a>
     </div>
   );
 }
